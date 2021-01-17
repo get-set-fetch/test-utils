f8611db4e8983dc1fb4c09ebe07465c251bf06d2
Andrei Sabau <andrei@asabau.com>
2021-01-17 19:33:18 +0200

    export typescript types

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,4 @@
-import GsfServer from './server/GsfServer';
-import ScrapingSuite from './scraping-suite/ScrapingSuite';
+export {default as GsfServer} from  './server/GsfServer';
+export {default as ScrapingSuite, IScrapingTest} from './scraping-suite/ScrapingSuite';
+export const extra = 'just a named export required for testing partial package import when bundling scraper plugins';
 
-const extra = 'just a named export required for testing partial package import when bundling scraper plugins';
-
-export {
-  GsfServer,
-  ScrapingSuite,
-  extra,
-}
