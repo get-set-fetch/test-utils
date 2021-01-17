c2a81a1e378ced6fc120d4e2e885cc83a74aafa5
Andrei Sabau <andrei@asabau.com>
2021-01-17 18:44:44 +0200

    named export used in testing

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,10 @@
 import GsfServer from './server/GsfServer';
 import ScrapingSuite from './scraping-suite/ScrapingSuite';
 
+const extra = 'just a named export required for testing partial package import when bundling scraper plugins';
+
 export {
   GsfServer,
-  ScrapingSuite
+  ScrapingSuite,
+  extra,
 }
