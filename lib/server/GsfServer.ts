4d91ddb8d3023dd60b7805af2d253dd5518424b7
Andrei Sabau <andrei@asabau.com>
2021-03-28 19:02:16 +0300

    support for gzip content encoding

--- lib/server/GsfServer.ts
+++ lib/server/GsfServer.ts
@@ -1,7 +1,10 @@
 import connect from 'connect';
 import serveStatic from 'serve-static';
 import vhost from 'vhost';
+import path from 'path';
 import http, { IncomingMessage, ServerResponse } from 'http';
+import fs from 'fs';
+import zlib from 'zlib';
 
 export interface IVirtualHost {
   hostname: string;
@@ -49,19 +52,19 @@ export default class GsfServer {
       const vhostApp = connect();
 
       vhostApp.use(this.redirect);
+      vhostApp.use(this.serveStaticCompressed(virtualHost.root));
       vhostApp.use(serveStatic(virtualHost.root, virtualHost.staticOpts));
 
       this.connApp.use(<any>vhost(virtualHost.hostname, vhostApp));
-    })
-
-  
+    });
   }
+
 
   /*
   redirect all /dir/redirect-page.html req to /dir/page.html
   some integration tests make use of this functionality to test the scraper redirect handling capabilities
   */
-  redirect(req: IncomingMessage, res: ServerResponse, next:  connect.NextFunction) {
+  redirect(req: IncomingMessage, res: ServerResponse, next: connect.NextFunction) {
     if (/redirect-/.test(req.url)) {
       const targetUrl = req.url.replace(/redirect-/, '');
       res.statusCode = 301;
@@ -73,8 +76,41 @@ export default class GsfServer {
     }
   }
 
+  serveStaticCompressed(rootDir: string) {
+    return (req: IncomingMessage, res: ServerResponse, next: connect.NextFunction) => {
+      // handle only gzip compression for existing files with extension
+      if (
+        /gzip/.test(req.headers["accept-encoding"].toString()) 
+        && fs.existsSync(path.join(rootDir, req.url))
+        && path.basename(path.join(rootDir, req.url)).split(".").length === 2
+        ) {
+        const ext = path.basename(path.join(rootDir, req.url)).split(".")[1];
+        let contentType;
+        switch (ext) {
+          case "html":
+            contentType = 'text/html';
+            break;
+          case "png":
+            contentType = 'image/png';
+            break;
+          case "pdf":
+            contentType = 'application/pdf';
+            break;
+        }
+
+        res.writeHead(200, {
+          'Content-Encoding': 'gzip',
+          'Content-Type': contentType
+        });
+        fs.createReadStream(path.join(rootDir, req.url)).pipe(zlib.createGzip()).pipe(res);
+      }
+      else {
+        next();
+      }
+    }
+  }
+
   stop() {
     this.http.close();
   }
-
 }
--- test/test-scraping-suite.ts
+++ test/test-scraping-suite.ts
@@ -1,6 +1,10 @@
-import assert from 'assert';
-import { ScrapingSuite } from '../lib';
-import { IScrapingDefinition } from '../lib/scraping-suite/ScrapingSuite';
+import { assert } from 'chai';
+import http from 'http';
+import path from 'path';
+import zlib from 'zlib';
+import { pipeline, Writable } from 'stream';
+import GsfServer from '../lib/server/GsfServer';
+import ScrapingSuite, { IScrapingDefinition } from '../lib/scraping-suite/ScrapingSuite';
 
 describe('ScrapingSuite', () => {
   it('getTests - count', () => {
@@ -28,5 +32,60 @@ describe('ScrapingSuite', () => {
 
     assert.deepStrictEqual(redirectTest.definition, expectedDefinition);
   });
+
+  it('serve static compressed', async () => {
+    const srv = new GsfServer();
+    srv.start();
+    
+    srv.update([{root: path.join(__dirname, 'resources'), hostname: "sitea.com"}]);
+
+    let resHeaders:http.IncomingHttpHeaders;
+
+    const result:Buffer = await new Promise((resolve, reject) => {
+      const req = http.request({ 
+        method: "GET",
+        host: '127.0.0.1', 
+        port: 8080, 
+        path: '/pageA.html', 
+        headers: { 
+          Host: 'sitea.com', 
+          'Accept-Encoding': 'gzip'
+        }
+      });
+
+      let chunks:Buffer[] = [];
+  
+      req.on('response', (response) => {
+        resHeaders = response.headers;
+        const output = new Writable({
+          write(chunk, encoding, done) {
+            chunks.push(Buffer.from(chunk));
+            done();
+          },
+        });
+      
+        const onComplete = (err) => {
+          if (err) {
+            reject (err);
+          }
+          else {
+            resolve(Buffer.concat(chunks))
+          }
+        };
+      
+        pipeline(response, zlib.createGunzip(), output, onComplete);
+      });
+
+      req.end();
+    });
+
+    assert.include(resHeaders, {
+      'content-encoding': 'gzip',
+      'content-type': 'text/html',
+    });
+    assert.strictEqual(result.toString("utf8"), '<body><p>a</p></body>');
+    
+    srv.stop();
+  })
 
 });
